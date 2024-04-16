b5ce253a5d9b17f1ea29e1ce790df23cbcc9cf3e
Daniel de Lima <59209081+daniel-de-lima0xa@users.noreply.github.com>
2024-04-16 02:26:34 -0300

    Update TelegramNmapBotHub.py

--- TelegramNmapBotHub.py
+++ TelegramNmapBotHub.py
@@ -1,32 +1,29 @@
-# -*- coding: utf-8 -*-
-
 import subprocess
 from telegram.ext import Updater, CommandHandler, CallbackContext
 from telegram import Update
 import logging
-import os
 
 # Configuração do logger
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Token do Telegram (substitua pelo seu)
+# Token do Telegram
 telegram_token = "TOKEN"
 
 # ID do grupo
-group_id = id_grupo
+group_id = "ID_DO_GRUPO"
 
 
 def start(update: Update, context: CallbackContext) -> None:
     """Envia mensagem de boas-vindas e instruções ao usuário."""
     user_id = update.message.from_user.id
     update.message.reply_text(
-        f"Olá, usuário {user_id}! Envie comandos do Nmap no formato /nmap <flags> <alvo>.\n\nExemplo: /nmap -sT scanme.nmap.org"
+        f"Olá, usuário {user_id}! Envie comandos das ferramentas no formato /ferramenta <argumentos>."
     )
 
 
-def nmap(update: Update, context: CallbackContext) -> None:
-    """Executa Nmap com base nos argumentos fornecidos e envia o resultado ao usuário."""
+def execute_tool(update: Update, context: CallbackContext) -> None:
+    """Executa a ferramenta com base nos argumentos fornecidos e envia o resultado ao usuário."""
     try:
         # Extraindo argumentos do comando
         args = context.args
@@ -34,27 +31,30 @@ def nmap(update: Update, context: CallbackContext) -> None:
         # Verificando se há argumentos
         if not args:
             update.message.reply_text(
-                "Por favor, forneça os argumentos do Nmap. Exemplo: /nmap -sT scanme.nmap.org"
+                "Por favor, forneça os argumentos da ferramenta."
             )
             return
 
-        # Construindo o comando Nmap
-        nmap_command = ["nmap"] + args
+        # Obtendo o nome da ferramenta a partir do comando
+        tool_name = context.args[0]
+
+        # Construindo o comando da ferramenta
+        tool_command = [tool_name] + args[1:]
 
         # Executando o comando e obtendo a saída
-        result = subprocess.check_output(nmap_command)
+        result = subprocess.check_output(tool_command)
 
         # Decodificando e formatando a saída
         formatted_result = result.decode("utf-8").replace("_", "\\_")
 
         # Enviando o resultado formatado para o usuário
         update.message.reply_text(
-            f"**Resultado Nmap**\n\n{formatted_result}\n", parse_mode="Markdown"
+            f"**Resultado da ferramenta {tool_name}**\n\n{formatted_result}\n", parse_mode="Markdown"
         )
     except subprocess.CalledProcessError as e:
-        logger.error(f"Erro ao executar o Nmap: {e.output.decode('utf-8')}")
+        logger.error(f"Erro ao executar a ferramenta: {e.output.decode('utf-8')}")
         update.message.reply_text(
-            "Erro ao executar o Nmap. Verifique os argumentos e tente novamente."
+            f"Erro ao executar a ferramenta. Verifique os argumentos e tente novamente."
         )
 
 
@@ -63,9 +63,12 @@ def main() -> None:
     updater = Updater(token=telegram_token, use_context=True)
     dp = updater.dispatcher
 
-    # Adicionando handlers para os comandos
+    # Adicionando handlers para os comandos das ferramentas
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("nmap", nmap, pass_args=True))
+    dp.add_handler(CommandHandler("katana", execute_tool, pass_args=True))
+    dp.add_handler(CommandHandler("httpx", execute_tool, pass_args=True))
+    dp.add_handler(CommandHandler("subfinder", execute_tool, pass_args=True))
+    dp.add_handler(CommandHandler("ffuf", execute_tool, pass_args=True))
 
     # Enviando mensagem para o grupo
     updater.bot.send_message(chat_id=group_id, text="Olá, grupo!")
